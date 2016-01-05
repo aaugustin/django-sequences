c8a03be847f8865a8687bc416e12d5e691217829
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-01-05 22:07:36 +0100

    Add packaging-related files.

---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+
+import setuptools
+
+# Avoid polluting the .tar.gz with ._* files under Mac OS X
+os.putenv('COPYFILE_DISABLE', 'true')
+
+# Prevent distutils from complaining that a standard file wasn't found
+README = os.path.join(os.path.dirname(__file__), 'README')
+if not os.path.exists(README):
+    os.symlink(README + '.rst', README)
+
+description = "Generate gap-less sequences of integer values."
+
+with open(README) as f:
+    long_description = '\n\n'.join(f.read().split('\n\n')[1:])
+
+setuptools.setup(
+    name='django-sequences',
+    version='1.0',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    url='https://github.com/aaugustin/django-sequences',
+    description=description,
+    long_description=long_description,
+    download_url='http://pypi.python.org/pypi/django-sequences',
+    packages=[
+        'sequences',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    platforms='all',
+    license='BSD'
+)
