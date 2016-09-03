5698121703c8adc54dc369a399aa8eb5bf21f5eb
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-09-03 20:17:09 +0200

    Simplify packaging according to PyPA guidelines.

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
-import os
+from __future__ import unicode_literals
+
+import codecs
+import os.path
 
 import setuptools
 
-# Avoid polluting the .tar.gz with ._* files under Mac OS X
-os.putenv('COPYFILE_DISABLE', 'true')
-
-# Prevent distutils from complaining that a standard file wasn't found
-README = os.path.join(os.path.dirname(__file__), 'README')
-if not os.path.exists(README):
-    os.symlink(README + '.rst', README)
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
 description = "Generate gap-less sequences of integer values."
-
-with open(README) as f:
-    long_description = '\n\n'.join(f.read().split('\n\n')[1:])
+with codecs.open(os.path.join(root_dir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='django-sequences',
     version='1.0.1',
-    author='Aymeric Augustin',
-    author_email='aymeric.augustin@m4x.org',
-    url='https://github.com/aaugustin/django-sequences',
     description=description,
     long_description=long_description,
-    download_url='http://pypi.python.org/pypi/django-sequences',
+    url='https://github.com/aaugustin/django-sequences',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=[
         'sequences',
         'sequences.migrations',
@@ -33,22 +45,4 @@ setuptools.setup(
             'locale/*/LC_MESSAGES/*',
         ],
     },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ],
-    platforms='all',
-    license='BSD'
 )
