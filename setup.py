8946891a081490cff0784350ef80c85b5718f7b1
Aymeric Augustin <aymeric.augustin@m4x.org>
2017-03-17 15:59:07 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open(os.path.join(root_dir, 'README.rst'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='django-sequences',
-    version='1.0.1',
+    version='2.0',
     description=description,
     long_description=long_description,
     url='https://github.com/aaugustin/django-sequences',
@@ -31,13 +31,12 @@ setuptools.setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=[
         'sequences',
